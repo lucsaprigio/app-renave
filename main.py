ef45f797713d21b2da335902c0e286742d5e0d60
Lucas Aprigio <lucsaprigio@hotmail.com>
2025-05-09 12:02:03 -0300

    Agendamento de tarefas

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import schedule
 import json
 import time
+
 from app.http.client_controller import enviar_clientes
 from app.repositories.clientes import ClientesRepository
+
+from app.http.vehicle_controller import enviar_veiculos
+from app.repositories.vehicles import VeiculosRepository
 
 def agendador_enviar_clientes():
     print('Executando Busca de clientes...')
@@ -10,10 +14,28 @@ def agendador_enviar_clientes():
     data = json.loads(clientes.get_clientes())
     enviar_clientes(data)
 
-schedule.every(1).minutes.do(agendador_enviar_clientes)
+def agendador_enviar_veiculos():
+    print('Executando Busca de veiculos...')
+    veiculos = VeiculosRepository()
+    data = json.loads(veiculos.get_veiculos())
+    enviar_veiculos(data)
+
+schedule.every(30).minutes.do(agendador_enviar_clientes)
+schedule.every(30).minutes.do(agendador_enviar_veiculos)
 
 print('Aplicação rodando...')
 
+# veiculos = VeiculosRepository()
+
+# data = json.loads(veiculos.get_veiculos())
+# enviar_veiculos(data)
+
+clientes = ClientesRepository()
+
+# data = json.loads(clientes.get_clientes())
+# enviar_clientes(data)
+
 while True:
     schedule.run_pending()
-    time.sleep(1)
+
+    time.sleep(3)
